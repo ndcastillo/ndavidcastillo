07935d962bd2b71c4c1bb68c1dadcea08b165c9a
David Castillo <davichitoc@gmail.com>
2026-04-06 16:23:12 +0000

    feat(blog): cambios de tiempos de lectura

--- src/content.config.ts
+++ src/content.config.ts
@@ -15,6 +15,7 @@ const blog = defineCollection({
 			updatedDate: z.coerce.date().optional(),
 			heroImage: z.optional(image()),
 			tags: z.array(z.string()).optional(),
+			draft: z.boolean().optional().default(false),
 		}),
 });
 
